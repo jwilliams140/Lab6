715f9c95552c80eec419d6f9e720e8f4a317ee9e
jwilliams140 <jwilliams140@csub.edu>
2026-04-26 13:05:44 -0700

    Home page will now function as student search

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import { useEffect, useState } from "react";
+import AnalyticsScreen from "./screens/AnalyticsScreen";
+import FormScreen from "./screens/FormScreen";
+import RiskScreen from "./screens/RiskScreen";
+import StudentsScreen from "./screens/StudentsScreen";
+import { loadStudents, saveStudents } from "./utilities/storage";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-    const [students, setStudents] = useState([]);
+  const [students, setStudents] = useState([]);
 
-    useEffect(() => {
-        async function getData() {
-            const data = await loadStudents();
-            if (data) setStudents(data);
-        }
-        getData();
-    }, []);
+  useEffect(() => {
+    async function getData() {
+      const data = await loadStudents();
+      if (data) setStudents(data);
+    }
+    getData();
+  }, []);
 
-    useEffect(() => {
-        saveStudents(students);
-    }, [students]);
+  useEffect(() => {
+    saveStudents(students);
+  }, [students]);
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Students">
+          {props => (
+            <StudentsScreen
+              {...props}
+              students={students}
+              setStudents={setStudents}
+            />
+          )}
+        </Tab.Screen>
+
+        <Tab.Screen name="Add/Edit">
+          {props => (
+            <FormScreen
+              {...props}
+              students={students}
+              setStudents={setStudents}
+            />
+          )}
+        </Tab.Screen>
+
+        <Tab.Screen name="Analytics">
+          {props => <AnalyticsScreen {...props} students={students} />}
+        </Tab.Screen>
+
+        <Tab.Screen name="Risk">
+          {props => <RiskScreen {...props} students={students} />}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
+
